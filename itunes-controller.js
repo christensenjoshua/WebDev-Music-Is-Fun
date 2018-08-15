17d425c31d57f15deffce82373ab71a0fdc43d1c
christensenjoshua <christensen.joshua@outlook.com>
2018-08-14 20:40:39 -0600

    basic functionality complete, need to add more advanced audio controls.

--- itunes-controller.js
+++ itunes-controller.js
@@ -2,20 +2,54 @@ function ItunesController() {
 
   // PRIVATE
   const itunesService = new ItunesService()
-
   function drawSongs(results) {
     console.log(results)
     //YOUR CODING STARTS HERE
-
-
-
+    let template = ``
+    for (let i = 0; i < results.length; i++) {
+      const item = results[i]
+      template += `
+      <div class="row">
+        <div class="col-2">
+          <img class="img-fluid" src="${item.albumArt}">
+        </div>
+        <div class="col-7">
+          <audio id="audio${i}">
+            <source src="${item.preview}" type="audio/mp4">
+          </audio>
+          <h5 onclick="app.controllers.itunesCtrl.playTrack('${i}')"><span id="track${i}">****</span>${item.title}</h5>
+          <h6>Album: ${item.collection}</h6>
+          <h6>By: ${item.artist}</h6>
+        </div>
+        <div class="col-3 text-right">
+          <h6>Price: $<span class="badge badge-info">${item.price}</span></h6>
+        </div>
+      </div>
+      `
+    }
+    document.getElementById('songs').innerHTML = template
   }
-
   //PUBLIC
+  this.playTrack = function (id) {
+    let elem = document.getElementById('audio' + id)
+    let imgElem = document.getElementById('track' + id)
+    if (!elem.paused) {
+      elem.pause()
+      //draw pause button since it's playing
+    } else {
+      let audios = document.getElementsByTagName('audio')
+      for (let i = 0, len = audios.length; i < len; i++) {
+        audios[i].pause()
+        // also draw each span as play since they should all be paused?
+      }
+      elem.play()
+      //draw play button since it's paused.
+    }
+  }
   //DO NOT MODIFY THIS METHOD
   this.getMusic = function (e) {
     e.preventDefault();
-    var artist = e.target.artist.value;
+    let artist = e.target.artist.value;
     //changes the button to loading while songs load
     $('#get-music-button').text('LOADING....');
     itunesService.getMusicByArtist(artist).then(results => {
--- itunes-service.js
+++ itunes-service.js
@@ -1,11 +1,10 @@
 function ItunesService() {
-
-  //DO NOT MODIFY
   this.getMusicByArtist = function (artist) {
-    var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
+    let url1 = '//bcw-getter.herokuapp.com/?url='
+    let url = 'https://itunes.apple.com/search?term=' + artist
     //Casts each object to 
     return $.getJSON(url).then(function (response) {
-      var songList = response.results.map(song => {
+      let songList = response.results.map(song => {
         return {
           title: song.trackName,
           albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
@@ -18,4 +17,23 @@ function ItunesService() {
       return songList;
     })
   }
+  
+  //DO NOT MODIFY
+  // this.getMusicByArtist = function (artist) {
+  //   var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
+  //   //Casts each object to 
+  //   return $.getJSON(url).then(function (response) {
+  //     var songList = response.results.map(song => {
+  //       return {
+  //         title: song.trackName,
+  //         albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
+  //         artist: song.artistName,
+  //         collection: song.collectionName,
+  //         price: song.collectionPrice,
+  //         preview: song.previewUrl,
+  //       }
+  //     })
+  //     return songList;
+  //   })
+  // }
 }
