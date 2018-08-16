8ae65d8cee6b1a13ff934a551ec15e9943938d04
christensenjoshua <christensen.joshua@outlook.com>
2018-08-15 19:19:47 -0600

    returned to previous function with callback

--- itunes-service.js
+++ itunes-service.js
@@ -1,7 +1,26 @@
 function ItunesService() {
+  // this.getMusicByArtist = function (artist) {
+  //   let url1 = '//bcw-getter.herokuapp.com/?url='
+  //   let url = 'https://itunes.apple.com/search?term=' + artist + '&media=music'
+  //   //Casts each object to 
+  //   return $.getJSON(url).then(function (response) {
+  //     let songList = response.results.map(song => {
+  //       return {
+  //         title: song.trackName,
+  //         albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
+  //         artist: song.artistName,
+  //         collection: song.collectionName,
+  //         price: song.collectionPrice,
+  //         preview: song.previewUrl,
+  //       }
+  //     })
+  //     return songList;
+  //   })
+  // }
+  
+  //DO NOT MODIFY
   this.getMusicByArtist = function (artist) {
-    let url1 = '//bcw-getter.herokuapp.com/?url='
-    let url = 'https://itunes.apple.com/search?term=' + artist + '&media=music'
+    var url = 'https://itunes.apple.com/search?term=' + artist + "&media=music&callback=?"
     //Casts each object to 
     return $.getJSON(url).then(function (response) {
       let songList = response.results.map(song => {
@@ -17,23 +36,4 @@ function ItunesService() {
       return songList;
     })
   }
-  
-  //DO NOT MODIFY
-  // this.getMusicByArtist = function (artist) {
-  //   var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
-  //   //Casts each object to 
-  //   return $.getJSON(url).then(function (response) {
-  //     var songList = response.results.map(song => {
-  //       return {
-  //         title: song.trackName,
-  //         albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
-  //         artist: song.artistName,
-  //         collection: song.collectionName,
-  //         price: song.collectionPrice,
-  //         preview: song.previewUrl,
-  //       }
-  //     })
-  //     return songList;
-  //   })
-  // }
 }
